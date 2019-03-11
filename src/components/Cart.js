524d41b8bfb097796213a502df6f63537fc3b86a
ivanbkovacevic <ivanbkovacevic@gmail.com>
2019-03-11 20:37:33 +0100

    first

---
+++ src/components/Cart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Cart extends Component {
+    render() {
+        return (
+            <div>
+                <h3>helo from cART</h3>
+            </div>
+        );
+    }
+}
+
+export default Cart;
